b47d195275d39201c6bd0da4c26acbae28fcbcb9
CrossNox <ijmermet@gmail.com>
2021-12-10 13:01:33 -0300

    pin

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except ImportError:
         readme = f.read()
 
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 install_requires = ["mistune==0.8.4", "docutils"]
 test_requirements = ["pygments"]
